0a883bd8dadac92267ad43bccff15ddc1f18a6a6
Anhar01 <152172958+Anhar01@users.noreply.github.com>
2025-06-12 14:33:26 +0800

    Update 6. Comparison Query.js

--- 6. Comparison Query.js
+++ 6. Comparison Query.js
@@ -1,18 +1,8 @@
 # 6. Comparison Query
 
-Digunakan untuk mencari data berdasarkan perbandingan.
 
-Contoh:
-```js
 db.pelanggan.find({ umur: { $gt: 25 } })      // Umur lebih dari 25
 
 db.pelanggan.find({ umur: { $in: [22, 30] } }) // Umur 22 atau 30
-```
 
-Operator umum:
-- `$eq`: sama dengan
-- `$ne`: tidak sama dengan
-- `$gt`: lebih besar dari
-- `$lt`: lebih kecil dari
-- `$in`: dalam daftar
-- `$nin`: tidak dalam daftar
+
