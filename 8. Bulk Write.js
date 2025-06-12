9ca790e91c0b4c2d4c6f8134b5cfab96f62fdc62
Anhar01 <152172958+Anhar01@users.noreply.github.com>
2025-06-12 14:34:48 +0800

    Update 8. Bulk Write.js

--- 8. Bulk Write.js
+++ 8. Bulk Write.js
@@ -1,12 +1,8 @@
 # 8. Bulk Write
 
-Melakukan banyak operasi sekaligus seperti insert, update, delete.
-
-Contoh:
-```js
 db.pelanggan.bulkWrite([
   { insertOne: { document: { nama: "Dewi", umur: 28 } } },
   { updateOne: { filter: { nama: "Budi" }, update: { $set: { umur: 32 } } } },
   { deleteOne: { filter: { nama: "Cici" } } }
 ])
-```
+
