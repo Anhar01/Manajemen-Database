13329a0fd03a8b3eb3b54be64aec76ddf8a8f8f2
Anhar01 <152172958+Anhar01@users.noreply.github.com>
2025-06-12 14:32:11 +0800

    Update 5. Delete Document.js

--- 5. Delete Document.js
+++ 5. Delete Document.js
@@ -1,10 +1,6 @@
 # 5. Delete Document
 
-Digunakan untuk menghapus dokumen dari collection.
-
-Contoh:
-```js
 db.pelanggan.deleteOne({ nama: "Cici" })
 
 db.pelanggan.deleteMany({ umur: { $gt: 60 } })
-```
+
