3c280f916cb8f3331af84754e0d6a3b63e61a629
Anhar01 <152172958+Anhar01@users.noreply.github.com>
2025-06-12 14:29:43 +0800

    Update 2. Create Document.js

--- 2. Create Document.js
+++ 2. Create Document.js
@@ -1,13 +1,9 @@
 # 2. Create Document
 
-Digunakan untuk menambahkan data (dokumen) ke dalam collection.
-
-Contoh:
-```js
 db.pelanggan.insertOne({ nama: "Ani", umur: 25 })
 
 db.pelanggan.insertMany([
   { nama: "Budi", umur: 30 },
   { nama: "Cici", umur: 22 }
 ])
-```
+
